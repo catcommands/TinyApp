6470c917df2c04bef6df7d111391517c876ee95c
JaffarShah <sjeffshah@gmail.com>
2019-04-10 16:51:50 -0400

    added urls index

--- express_server.js
+++ express_server.js
@@ -21,10 +21,10 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
-// app.get("/", (req, res) => {
-//   let templateVars = { urls: urlDatabase };
-//   res.render("urls_index", templateVars);
-// });
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   let templateVars = { greeting: 'Hello World!' };
