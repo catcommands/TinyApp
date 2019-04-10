1308fccbac06f9a11e33ad46464e50902f2b34dc
JaffarShah <sjeffshah@gmail.com>
2019-04-10 16:06:27 -0400

    add render, url

--- express_server.js
+++ express_server.js
@@ -21,9 +21,14 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
 });
 
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+// app.get("/", (req, res) => {
+//   let templateVars = { urls: urlDatabase };
+//   res.render("urls_index", templateVars);
+// });
+
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
 // result in browser: Hello!
